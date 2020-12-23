6c3151dae962d708fea777506f989c8918c632c7
jasonouyang8000 <jianjun.ouyang@lacity.org>
2020-12-23 15:56:49 -0800

    first commit

---
+++ src/script.js
@@ -0,0 +1,22 @@
+class StockPortfolio extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (   
+            <div className="container">
+
+            </div>
+        );
+    }
+}
+
+
+ReactDOM.render(
+    <StockPortfolio />,
+    document.getElementById('root')
+);
